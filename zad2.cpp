cff45cd9769b5fd276b068c7154b7db5ea105fd6
Patryk Kośmider <98033934+Kosiem@users.noreply.github.com>
2022-12-18 17:37:16 +0100

    Update zad2.cpp

--- zad2.cpp
+++ zad2.cpp
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <iostream>
 
@@ -24,6 +17,6 @@ int main()
     } else{
         cout << x << " jest rowne 0";
     }
-   return 0
+   return 0;
 
 }
